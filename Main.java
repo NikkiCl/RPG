9afe5ad38f5df25bce01ae64c023acd5544313e3
Nikki Cai <168057238+NikkiCl@users.noreply.github.com>
2024-10-02 19:12:08 +1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,19 +6,35 @@ public class Main {
 	
 	
 	static void MainMenu() throws InterruptedException {
+		Scanner imp = new Scanner(System.in);
+		int userInp;
 		DPO("Welcome to RPG game name here idk",10);
 		System.out.println("\n\n1. Start game\n2. Quit");
+		userInp = imp.nextInt();
+		
+		if (userInp == 1) {
+			startGame();
+		}
 	}
-
+	
+	static void startGame() throws InterruptedException {
+		DPO("EMERGENCY!!!!\nEMERGENCY!!!\nFUEL TANK LOW, PLEASE REFILL....",20);
+		DPO("The system's warnings blared through the spaceship speakers, with flashing red and orange lights beginning to fill the room.",10);
+		DPO("Suddenly, a human voice rings through the speaker.",10);
+		DPO("'You alright man? I can see your gas tank's a little low.'",10);
+		DPO("You let out a sigh of relief, realising it's your coworker Dave",10);
+		DPO("'Yeah, I'm alright, just need to take a slight detour'",10);
+		//DPO("The word 'detour' had become integrated slang within our workplace, ")
+	}
 
     static void combat(Character X, Alien Enemy) throws InterruptedException {
         int userInp = 0;
+        Scanner imp = new Scanner(System.in);
 
         //combat loop
         while (X.getHP() > 0 && Enemy.getHP() > 0) {
         	//while main character HP and enemy HP arent below 0:
             try {
-                Scanner imp = new Scanner(System.in);
                 System.out.println("\n\n\n");
                 DPO("What would you like to do?",10);
                 DPO("1. Fight	2. Special Attack	3. Sneak	4. Use item\n",10);
@@ -26,7 +42,7 @@ public class Main {
                 //if user inputs 1, they fight, else 2, they sneak.
                 userInp = imp.nextInt();
                 if (userInp == 1) {
-                    X.fight(Enemy);
+                    Attack(X,Enemy);
                     //if the enemy gets slain, it skips the enemy's turn and goes straight to the cutscene
                     DPO("You slash " + Enemy.getName() + " with your sword and deal " + X.getDmg() + " damage.",10);
                     if (Enemy.getHP() < 0) {
@@ -68,12 +84,7 @@ public class Main {
                 Thread.sleep(400);
 
                 //enemy's turn.
-                if(Enemy.Afight(X)) {
-                    DPO("Enemy " + Enemy.getName() + " has dealt " + Enemy.getDmg() + " damage",10);
-                }
-                else {
-                    DPO("Enemy " + Enemy.getName() + " is charging up their attack...",10);
-                }
+                Attack(Enemy,X);
 
                 if(Enemy.getCounter() > 0) {
                 	SpecialAttack(X,Enemy);
@@ -97,13 +108,20 @@ public class Main {
             DPO("Enemy " + Enemy.getName() + " has been slained!",10 );
             DPO(Enemy.getName() + " has dropped a " + Enemy.getItem(),10);
         }
+        imp.close();
     }
-
-
+    
+    
+    
+//=--------------------------------------------------------------------
+    //MAIN 
+    
+    
+    
     public static void main(String[] args) throws InterruptedException {
     	MainMenu();
         Character t1 = new Character("n",10,100);
-        Alien t2 = new Alien("f",10,100,2,"sword");
+        Alien t2 = new Alien("f",10,100,2,2,"sword");
 
         t1.setSPA("Poison");
 
@@ -111,14 +129,45 @@ public class Main {
 
     }
     
+    
+    //-----------------------------------------------
+    
+    
+    
+    
+    
+    static boolean Attack(Character X, Character Enemy) throws InterruptedException {
+    	if (X.getTTA() == 0) {
+            DPO("Enemy " + Enemy.getName() + " has dealt " + Enemy.getDmg() + " damage",10);
+	    	X.dmg(Enemy);
+	    	return true;
+    	}
+    	else {
+    		if (X.isFrozen()) {
+	    		DPO(X.getName() + " is currently frozen for " + X.getTTA() + " turn(s)",10);
+	    		X.setTTA(X.getTTA() - 1);
+	    	}
+    		else if (X.getTTA() > 0) {
+                DPO("Enemy " + Enemy.getName() + " is charging up their attack...",10);
+    		}
+    	}
+    	return false;
+    	
+    }
+    
     static void SpecialAttack(Character X, Character Enemy) throws InterruptedException {
-        	if (X.getSPA() == "Poison") {
+        	if (X.getSPA().equals("Poison")) {
             DPO("Enemy is poisoned... take 5 damage",10);
             Enemy.setHP(Enemy.getHP() - X.getSpecialAttacks().get(X.getSPA()));
-            Enemy.setCounter(Enemy.getCounter() - 1);
-        
+
         	}
-        	//This is the special attack function, enemy -= 5
+        	else if (X.getSPA().equals("Frozen")) {
+        		DPO("Enemy is frozen for " + X.getSpecialAttacks().get(X.getSPA()) + " turns",10);
+        		
+        	}
+        	
+            Enemy.setCounter(Enemy.getCounter() - 1);
+        	//This is the special attack function, enemy -= 5 if poison 
         	//enemycounter -- 
     }
     static void DPO(String str, long delay) throws InterruptedException {
