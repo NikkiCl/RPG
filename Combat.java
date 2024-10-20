c598edaa3b9b8e15ad6cbbf37f0746e406fef584
Nikki Cai <168057238+NikkiCl@users.noreply.github.com>
2024-10-20 15:45:54 +1100

    Update Combat.java

--- Combat.java
+++ Combat.java
@@ -14,7 +14,7 @@ public class Combat {
 	            try {
 	                System.out.println("\n\n\n");
 	                DPO("What would you like to do?",10);
-	                DPO("1. Fight	2. Special Attack	3. Sneak	4. Use item\n",10);
+	                DPO("1 - Fight		2 - Sneak	3 - Use item\n",10);
 	                TimeUnit.SECONDS.sleep(2);
 	                //if user inputs 1, they fight, else 2, they sneak.
 	                userInp = imp.nextInt();
@@ -27,20 +27,6 @@ public class Combat {
 	                }
 
 	                else if (userInp == 2) {
-	                    X.StartSPA(Enemy);
-	                    if (X.getSPA().equals("Poison")) {
-	                    	DPO("You poison " + Enemy.getName() + " with your magic and deal " + X.getSpecialAttacks().get(X.getSPA()) + " damage.",10);
-	                    	}
-	                    else if (X.getSPA().equals("Ice")) {
-	                    	DPO("You freeze " + Enemy.getName() + " with your alien powers for " + X.getSpecialAttacks().get(X.getSPA()) + " turns",10);
-	                    	Enemy.setFrozen(true);
-	                    }
-	                    if (Enemy.getHP() < 0) {
-	                        continue;
-	                    }
-	                }
-
-	                else if (userInp == 3) {
 	                	//This is the sneak chance, 1 in 3 chance of getting away.
 	                    int sneakCh = ThreadLocalRandom.current().nextInt(1,4);
 
@@ -54,7 +40,7 @@ public class Combat {
 	                    }
 	                }
 
-	                else if (userInp == 4) {
+	                else if (userInp == 3) {
 	                	//should check for inventory before allowing to use item.
 	                }
 
@@ -68,10 +54,6 @@ public class Combat {
 	                //enemy's turn.
 	                Attack(Enemy,X);
 	                
-
-	                if(Enemy.getCounter() > 0) {
-	                	SpecialAttack(X,Enemy);
-	                }
 
 	                Thread.sleep(400);
 	                System.out.println("\nHP: " + X.getHP());
@@ -95,40 +77,21 @@ public class Combat {
 	    
     
     static boolean Attack(Character X, Character Enemy) throws InterruptedException {
+    	//if the turns to attack are 0, then they can attack.
     	if (X.getTTA() == 0) {
 	    	X.dmg(Enemy);
 	    	DPO(X.getName() + " attacks " + Enemy.getName() + " and deals " + X.getDmg() + " damage",10);
 	    	return true;
     	}
     	else {
-    		if (X.isFrozen()) {
-	    		DPO(X.getName() + " is currently frozen for " + X.getTTA() + " turn(s)",10);
-	    	}
-    		else if (X.getTTA() > 0) {
-                DPO("Enemy " + Enemy.getName() + " is charging up their attack...",10);
+             DPO("Enemy " + Enemy.getName() + " is charging up their attack...",10);
     		}
-            X.setTTA(X.getTTA() - 1);
-    	}
+    		//TTA--
+        X.setTTA(X.getTTA() - 1);
     	return false;
     	
     }
-    
-    static void SpecialAttack(Character X, Character Enemy) throws InterruptedException {
-        	if (X.getSPA().equals("Poison")) {
-            DPO(Enemy.getName() + " is poisoned... take 5 damage",10);
-            Enemy.setHP(Enemy.getHP() - X.getSpecialAttacks().get(X.getSPA()));
-
-        	}
-        	else if (X.getSPA().equals("Frozen")) {
-        		DPO(Enemy.getName() + " is frozen for " + X.getSpecialAttacks().get(X.getSPA()) + " turns",10);
-        		
-        	}
-        	
-            Enemy.setCounter(Enemy.getCounter() - 1);
-        	//This is the special attack function, enemy -= 5 if poison 
-        	//enemycounter -- 
-    }
-    
+        
     static void DPO(String str, long delay) throws InterruptedException {
     	//for character in string that has been turned into a list, print out the character after a certain amount of milliseconds.
         System.out.println();
