8157ff97dc20a9952f4cfee5a1c2567614d9123a
Nikki Cai <168057238+NikkiCl@users.noreply.github.com>
2024-10-26 14:49:11 +1100

    Update Main.java

--- Main.java
+++ Main.java
@@ -463,12 +463,12 @@ public class Main{
     }
 
 	static void wingedCreatureMiniBoss(Character x) throws InterruptedException{
-		Alien jock = new Alien("Jock Bird",100,100,0,"Antenna");
+		Alien jock = new Alien("Jock Bird",50,50,0,"Antenna");
 		Combat.combat(x, jock);
 	}
 
 	static void finalBoss(Character x) throws InterruptedException{
-		Alien finalBoss = new Alien("Blarbazop",50,100,2,"Crown");
+		Alien finalBoss = new Alien("Blarbazop",75,120,2,"Crown");
 		Combat.combat(x,finalBoss);
 	}
 
