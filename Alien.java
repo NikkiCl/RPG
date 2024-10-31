3d9fb5b2658081acca9b4406656b14a93688cece
Nikki Cai <168057238+NikkiCl@users.noreply.github.com>
2024-11-01 09:15:53 +1100

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -1,21 +1,21 @@
 public class Alien extends Character {
-	private int attackTime;
 	private String itemDropped;
+	
 	//subclass
+
+
+	//constructor
 	public Alien(String name, int damage,int HP, int turnsToAttack, String itemDropped) {
 		super(name,damage,HP,turnsToAttack);
+		//inherits all the variables from the character class 
 		this.itemDropped = itemDropped;
 	}
 	
 	
 	
-	
+	//getters 
 	public String getItem() {
 		return itemDropped;
-	}
-	
-	public int getAttackTime() {
-		return attackTime;
 	}
 	
 }
